08721487406906c7642e186ed82294e46994e522
Scinarf <franciswilliamson@gmail.com>
2023-02-18 13:53:45 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
-#!/bin/bash
-
 #include <stdio.h>
 
-int main(void){
-	echo "Programming is like building a multilingual puzzle" > a;
-	puts(a);
-	return 0;
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
