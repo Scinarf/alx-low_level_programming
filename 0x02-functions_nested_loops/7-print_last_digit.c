2641ce35f1f4d25ef6f5579259cbf1d24074e0c1
Scinarf <franciswilliamson@gmail.com>
2023-02-21 19:23:44 -0800

    print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * print_last_digit - prints the last digit of a number
+  * @n: wthis is where we get the last number from
+  * Return: Alwasys 0 (Success)
+  */
+
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+	{
+		n = -n;
+	}
+	
+	a = n % 10;
+
+	if (a < 0)
+	{
+		a = -a;
+	}
+	_putchar(a + '0');
+
+	return (a);
+
+}
