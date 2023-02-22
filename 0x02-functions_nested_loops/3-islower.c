e529c0936c4c3117ce25fbb21af7d50c3ada4aa9
Scinarf <franciswilliamson@gmail.com>
2023-02-21 17:14:55 -0800

    Islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _islower - Entry point
+  * checking for lowercase characters
+  * @c: is the character being tested
+  * Return: Always 0 (Success)
+  */
+
+int _islower(int c)
+{
+	int a;
+
+	a = _islower(c);
+
+	if (a > 0)
+	{
+		return (1);
+	}
+	return (0);
+
+}
