7facbdbd8ffb8bd74430458e9d369c226848fcd4
Scinarf <franciswilliamson@gmail.com>
2023-02-24 10:29:56 -0800

    Mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * mul - This function is used to multiply two values
+  * @a: this variable holds values one
+  * @b: this variable holds value two
+  * Return: 0
+  */
+
+int mul(int a, int b)
+{
+	int ans = (a * b);
+
+	return (ans);
+}
