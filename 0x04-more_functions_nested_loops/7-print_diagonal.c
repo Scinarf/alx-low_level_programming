3350f0c561b5c730c7b33b1ca4d2639c8d735853
Scinarf <franciswilliamson@gmail.com>
2023-02-24 14:15:37 -0800

    Printing dash char

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: tells us how many characters to print out
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					putchar('\\');
+				else if (j < i)
+					putchar(' ');
+			}
+			putchar('\n');
+		}
+	}
+}
+
