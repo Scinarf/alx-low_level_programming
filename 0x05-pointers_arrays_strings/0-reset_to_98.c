878dbc180ca8bd9316aaaf4f61d288a518fda3d4
Scinarf <franciswilliamson@gmail.com>
2023-02-27 07:45:25 -0800

    Reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - using to reset the value to 98
+  * @n: The parameter we will use to reset the value
+  * Return: void
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
