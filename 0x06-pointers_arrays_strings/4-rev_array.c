6a1e6e9dce00c7e820868d55bd9d162851c06502
Scinarf <franciswilliamson@gmail.com>
2023-03-02 13:47:44 -0800

    Reversing an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,10 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+  * reverse_array - reversing an array
+  * @a: Holding the array
+  * @n: holds the array length
+  * Return: void
+  */
+
 void reverse_array(int *a, int n)
 {
 	int v;
 	int b;
+
 	for (v = 0; v < (n - 1); v++)
 	{
 		n--;
