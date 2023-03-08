7079dfcf803b33300a1d7de6189f540d4fb881fe
Scinarf <franciswilliamson@gmail.com>
2023-03-08 08:05:28 -0800

    Puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+  * _puts_recursion - Prints out a string using the puts function
+  * @s: Holds the string we want to print out
+  * Return: void
+  */
+
 void _puts_recursion(char *s)
 {
 
