2616049c5034c67205f4399edb4aa96268dcd74b
Scinarf <franciswilliamson@gmail.com>
2023-03-13 14:06:22 -0700

    first Malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * main - returns either NULL or a pointer
+  * @size: size for memory allocation
+  * @c: Holding character
+  * Return: NULL or a pointer
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int p;
+	char *ptr;
+
+	ptr = (char *)malloc(size*sizeof(char));
+
+	if (size == 0 || ptr == NULL)
+		return (NULL);
+
+	for (p = 0; p < size; p++)
+	{
+		ptr[p] = c;
+	}
+
+	return (ptr);
+}
