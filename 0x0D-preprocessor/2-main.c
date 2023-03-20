1846ea5e772965d06b5a327343a73c4340f49a81
Scinarf <franciswilliamson@gmail.com>
2023-03-20 12:39:48 -0700

    __FILE__

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * main - prints out the name of the current file
+  * Return: 0
+  */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
