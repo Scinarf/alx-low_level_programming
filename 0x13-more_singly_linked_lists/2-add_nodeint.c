3cb7206fc2475e17b3602e55a2e7a2d778640a0d
Romaric250 <lonfonyuyromeo@gmail.com>
2023-04-04 10:15:02 -0700

    add values to our list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h";
+
+/**
+  * add_nodeint - adds values to the beginning of our list
+  * @head: holds the address to our list
+  * @n: holds the value we want to put in our list
+  * Return: the address to our new list
+  */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *ptr = malloc(sizeof(listint_t));
+
+	if (ptr == NULL)
+		return (0);
+
+	ptr->n = n;
+	ptr->next = *head;
+
+	*head = ptr;
+
+	return (*head);
+}
