25355fad8014b73c0d28e1d446148cd696a873f1
Romaric250 <lonfonyuyromeo@gmail.com>
2023-04-04 02:46:28 -0700

    Printing a linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,13 +13,13 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (h -> str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 
 		else
 			printf("[%u] %s\n", h->len, h->str);
 
-		h = h -> next;
+		h = h->next;
 		a++;
 	}
 	return (a);
